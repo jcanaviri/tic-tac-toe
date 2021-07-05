344ad0ff5390703640dedd89b2c9b276934eb3c7
jcanaviri <jcanaviri20@gmail.com>
2021-07-05 18:07:35 -0400

    function for showing or sign on bot select

--- script.js
+++ script.js
@@ -5,25 +5,59 @@ const selectOBtn = document.querySelector('.playerO');
 const playerBoard = document.querySelector('.play-board');
 const players = document.querySelector('.players');
 
-const allBoxes = document.querySelectorAll('section span');
+const allBox = document.querySelectorAll('section span');
 
 let playerXIcon = '<ion-icon name="close-outline"></ion-icon>';
 let playerOIcon = '<ion-icon name="radio-button-off-outline"></ion-icon>';
+let playerSign = 'X';
+
+const bot = () => {
+  playerSign = 'O';
+  let array = [];
+  for (let i = 0; i < allBox.length; i++) {
+    if (allBox[i].childElementCount == 0) {
+      //if the box/span has no children means <i> tag
+      array.push(i); //inserting unclicked boxes number/index inside array
+    }
+  }
+  let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
+  if (array.length > 0) {
+    //if array length is greater than 0
+    if (players.classList.contains('player')) {
+      allBox[randomBox].innerHTML = playerXIcon; //adding cross icon tag inside bot selected element
+      players.classList.remove('active');
+      playerSign = 'X';
+      allBox[randomBox].setAttribute('id', playerSign);
+    } else {
+      allBox[randomBox].innerHTML = playerOIcon; //adding circle icon tag inside bot selected element
+      players.classList.remove('active');
+      allBox[randomBox].setAttribute('id', playerSign);
+    }
+  }
+  allBox[randomBox].style.pointerEvents = 'none';
+};
 
 // User click function
 const clickedBox = (item) => {
   if (players.classList.contains('player')) {
     item.innerHTML = playerOIcon;
     players.classList.add('active');
+    playerSign = 'O';
+    item.setAttribute('id', playerSign);
   } else {
     item.innerHTML = playerXIcon;
     players.classList.add('active');
+    item.setAttribute('id', playerSign);
   }
   item.style.pointerEvents = 'none';
+  let randomDelayTime = (Math.random() * 1000 + 200).toFixed();
+  setTimeout(() => {
+    bot();
+  }, randomDelayTime);
 };
 
 window.onload = () => {
-  allBoxes.forEach((box) => {
+  allBox.forEach((box) => {
     box.setAttribute('onclick', 'clickedBox(this)');
   });
 
